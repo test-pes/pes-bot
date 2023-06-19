3a265160989ef00cd0c943b6573718e0b4ffb406
Laaouatni Anas <laaouatni.anas@outlook.it>
2023-06-19 21:42:00 +0200

    save

--- logic_bot.ts
+++ logic_bot.ts
@@ -1,5 +1,40 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf } from "telegraf";
 
 export default function botLogic() {
-  
+  const bot = new Telegraf("5928046156:AAFbqK2oaYG5AKse1WiOkKUWItzqqgYCIcc");
+
+  bot.start((ctx) => ctx.reply("Welcome!"));
+  bot.help((ctx) => ctx.reply("Send me a sticker"));
+  bot.on("sticker", (ctx) => ctx.reply("👍"));
+  bot.hears("hi", (ctx) => ctx.reply("Hey there!"));
+
+  bot.hears("test", (ctx) => {
+    // create poll
+    ctx.reply("partecipi al campionato pes?", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: "si", callback_data: "1" },
+            { text: "no", callback_data: "0" },
+          ],
+        ],
+      },
+    });
+
+    // handle callback
+    bot.action("1", (ctx) => {
+      ctx.answerCbQuery("hai scelto si");
+      ctx.reply("ok, partecipi");
+      ctx.reply(`il nome è ${JSON.stringify(ctx.from)}`);
+      // tag the ctx.from.username
+      ctx.reply(`@${ctx?.from?.username} partecipa al campionato`);
+
+    });
+    bot.action("0", (ctx) => {
+      ctx.answerCbQuery("hai scelto no");
+      ctx.reply("ok, non partecipi");
+    });
+  });
+
+  bot.launch();
 }
--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import botLogic from "./logic_bot";
 import express from "express";
 const app = express();
 const PORT = process.env.PORT || 4000;
-// app.get("/", (req, res) => res.send("Hello from server!"));
+app.get("/", (req, res) => res.send("Hello from server!"));
 app.listen(PORT, () => console.log(`⚡Server is running here 👉 https://localhost:${PORT}`));
 
 botLogic();
