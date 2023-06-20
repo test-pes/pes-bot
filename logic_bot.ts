01f4b05718569c167febafd737a5ee9674b2b293
Laaouatni Anas <laaouatni.anas@outlook.it>
2023-06-20 13:12:23 +0200

    save

--- logic_bot.ts
+++ logic_bot.ts
@@ -1,40 +1,71 @@
+// @ts-nocheck
+
 import { Telegraf } from "telegraf";
+
+import mongoose from "mongoose";
+import { on } from "events";
+
+const uri =
+  "mongodb+srv://testtelegram:72756f0408@pesbot.kixclcz.mongodb.net/?retryWrites=true&w=majority";
 
 export default function botLogic() {
   const bot = new Telegraf("5928046156:AAFbqK2oaYG5AKse1WiOkKUWItzqqgYCIcc");
 
-  bot.start((ctx) => ctx.reply("Welcome!"));
-  bot.help((ctx) => ctx.reply("Send me a sticker"));
-  bot.on("sticker", (ctx) => ctx.reply("👍"));
-  bot.hears("hi", (ctx) => ctx.reply("Hey there!"));
+  // const arrayPartecipanti = [];
+  // bot.start((ctx) => ctx.reply("Welcome!"));
+  // bot.help((ctx) => ctx.reply("Send me a sticker"));
+  // bot.on("sticker", (ctx) => ctx.reply("👍"));
+  // bot.hears("hi", (ctx) => ctx.reply("Hey there!"));
+  // bot.hears("all", (ctx) =>
+  //   ctx.reply(
+  //     `ecco tutti i partecipanti: ${
+  //       arrayPartecipanti.length === 0
+  //         ? "*nessuno attualmente*".toUpperCase()
+  //         : JSON.stringify(arrayPartecipanti)
+  //     }`,
+  //   ),
+  // );
 
-  bot.hears("test", (ctx) => {
-    // create poll
-    ctx.reply("partecipi al campionato pes?", {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            { text: "si", callback_data: "1" },
-            { text: "no", callback_data: "0" },
-          ],
-        ],
-      },
-    });
+  // bot.hears("test", (ctx) => {
+  //   // create poll
+  //   ctx.reply("partecipi al campionato pes?", {
+  //     reply_markup: {
+  //       inline_keyboard: [
+  //         [
+  //           { text: "si", callback_data: "1" },
+  //           { text: "no", callback_data: "0" },
+  //         ],
+  //       ],
+  //     },
+  //   });
 
-    // handle callback
-    bot.action("1", (ctx) => {
-      ctx.answerCbQuery("hai scelto si");
-      ctx.reply("ok, partecipi");
-      ctx.reply(`il nome è ${JSON.stringify(ctx.from)}`);
-      // tag the ctx.from.username
-      ctx.reply(`@${ctx?.from?.username} partecipa al campionato`);
+  //   bot.action("1", (ctx) => {
+  //     ctx.reply(`il nome è ${JSON.stringify(ctx.from)}`);
 
-    });
-    bot.action("0", (ctx) => {
-      ctx.answerCbQuery("hai scelto no");
-      ctx.reply("ok, non partecipi");
-    });
-  });
+  //     arrayPartecipanti.push(ctx.from);
+  //     ctx.reply(`@${ctx?.from?.username} partecipa al campionato`);
+  //   });
+  //   bot.action("0", (ctx) => {
+  //     ctx.reply("ok, non partecipi");
+  //   });
+  // });
+
+  bot.hears("sondaggio", (ctx) => {
+    ctx.sendPoll("domanda a caso", ["risposta 1 SI", "risposta 2"], {
+      is_anonymous: false
+    })
+    // ctx.replyWithQuiz("domanda a caso", ["risposta 1 SI", "risposta 2"] )
+    // ctx.reply("reply normale, per vedere se il sondaggio reply è il problema oppure il codice id in generale che da l'errore")
+    // bot.command('poll', (ctx) =>{
+    //   let abc = ["Juice", "Tea"]
+    //   ctx.replyWithPoll(
+    //     'Your Fav Drink',
+    //     abc,
+    //     { is_anonymous: false }
+    //   )
+    // })
+    // bot.on('poll_answer', (ctx) => bot.reply(ctx.pollAnswer))
+  })
 
   bot.launch();
 }
--- server.ts
+++ server.ts
@@ -1,9 +1,13 @@
 import botLogic from "./logic_bot";
 
 import express from "express";
+import firebaseLogic from "./logic_firebase";
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.get("/", (req, res) => res.send("Hello from server!"));
-app.listen(PORT, () => console.log(`⚡Server is running here 👉 https://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`⚡Server is running here 👉 https://localhost:${PORT}`),
+);
 
+firebaseLogic();
 botLogic();
